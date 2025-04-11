0c8c8a1f60201de119a9dc91a4b99cfeb93cedfa
Pablo Ruiz <pablo_ruiza@hotmail.com>
2025-04-10 23:46:23 -0500

    configuration of sequalize file

--- src/database/sequalize.js
+++ src/database/sequalize.js
@@ -1,9 +1,6 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config';
 
-import { Sequelize } from "sequelize";
-import 'dotenv/config';
-
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
